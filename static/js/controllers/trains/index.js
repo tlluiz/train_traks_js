9b13d1964acdf537834c6083ef16e94c1128a3fb
Thiago Luiz <tluiz@thoughtworks.com>
2018-01-22 09:58:37 -0200

    Create the last part of system that is the jquery intergration between html form and tain trakc module.

---
+++ static/js/controllers/trains/index.js
@@ -0,0 +1,23 @@
+const scope = ".trip-form";
+
+$(scope).submit(function (e) {
+  e.preventDefault();
+  var stringTracks = $("input[name='stringTracks']", scope).val().split(",");
+  var trip = $("input[name='trip']", scope).val().toString();
+
+  var trips = new Trips(stringTracks);
+  var tripDistance = trips.distanceOfTheRoute(trip);
+
+  let tripResult = new TripResult(tripDistance);
+  tripResult.display(".trip-result");
+});
+
+class TripResult {
+  constructor(tripDistance, target) {
+    this.tripDistance = tripDistance;
+  }
+
+  display(target) {
+    $(target).show().html(this.tripDistance);
+  }
+}
